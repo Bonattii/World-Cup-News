62b4b044daba3f41e4fe12b93fbbf9ce49e55326
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2022-12-16 16:23:36 -0800

    Configure colors scheme in tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,15 @@ import logo from './assets/logo.svg';
 
 function App() {
   return (
-    <>
+    <div>
       <a href="/">
         <img src={logo} alt="" />
       </a>
       <h1>Hello World!</h1>
-    </>
+    </div>
   );
 }
 
 export default App;
-
 
 // SOCIAL MEDIAS ON THE NAVBAR AND A LINK TO SIGN UP ON THE NEWSLETTER
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.tsx', './index.html'],
   theme: {
-    extend: {}
+    colors: {
+      primary: '#8a1538',
+      primary10: '#b61c4a',
+      primary30: '#e5537f',
+      primary50: '#f2acc1',
+      primary_alpha: 'rgba(138, 21, 56, 0.3)',
+      secondary: '#ff004c',
+      accent: '#00cfb7',
+      background: '#eeeee4'
+    }
   },
   plugins: []
 };
