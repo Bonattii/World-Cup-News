d2e35935e13f2793224c54acdca3d60387640365
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2022-12-18 23:55:06 -0800

    Finish carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,76 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/autoplay';
+
+import { Autoplay, Navigation } from 'swiper';
+
 import Navbar from './components/Navbar';
 import ImportantNews from './components/ImportantNews';
 
 function App() {
   return (
-    <div className="bg-background h-screen w-full">
+    <div className="bg-background h-fyll w-full">
       <Navbar />
       <header>
         <div className="container mx-auto flex justify-center">
-          <h1 className="text-5xl mt-6 mb-4 text-primary font-bold">
+          <h1 className="text-5xl mt-8 mb-2 text-primary font-bold">
             Stay on top of all the world cup news!
           </h1>
         </div>
       </header>
-      <ImportantNews
-        bannerUrl="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2022/03/26/16482599142258.jpg"
-        title="Will Messi finally win a World Cup Title?"
-        description="That Messi is one of the best players in the world is something that we all known already, but this is going to be the time that he will pass Maradona and win finally a World Cup title with the Argentina National Team?"
-        altDesc="Messi with a big smile after going to the World Cup Final"
-      />
+      <Swiper
+        navigation={true}
+        modules={[Navigation, Autoplay]}
+        effect="fade"
+        className="mySwiper"
+        autoplay={{ disableOnInteraction: false, delay: 7000 }}
+        slidesPerView={1.5}
+        centeredSlides={true}
+        spaceBetween={100}
+      >
+        <SwiperSlide className="w-[60rem]">
+          <ImportantNews
+            bannerUrl="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2022/03/26/16482599142258.jpg"
+            title="Will Messi finally win a World Cup Title?"
+            description="That Messi is one of the best players in the world is something that we all known already, but this is going to be the time that he will pass Maradona and win finally a World Cup title with the Argentina National Team?"
+            altDesc="Messi with a big smile after going to the World Cup Final"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="w-[60rem]">
+          <ImportantNews
+            bannerUrl="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2022/03/26/16482599142258.jpg"
+            title="Will Messi finally win a World Cup Title?"
+            description="That Messi is one of the best players in the world is something that we all known already, but this is going to be the time that he will pass Maradona and win finally a World Cup title with the Argentina National Team?"
+            altDesc="Messi with a big smile after going to the World Cup Final"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="w-[60rem]">
+          <ImportantNews
+            bannerUrl="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2022/03/26/16482599142258.jpg"
+            title="Will Messi finally win a World Cup Title?"
+            description="That Messi is one of the best players in the world is something that we all known already, but this is going to be the time that he will pass Maradona and win finally a World Cup title with the Argentina National Team?"
+            altDesc="Messi with a big smile after going to the World Cup Final"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="w-[60rem]">
+          <ImportantNews
+            bannerUrl="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2022/03/26/16482599142258.jpg"
+            title="Will Messi finally win a World Cup Title?"
+            description="That Messi is one of the best players in the world is something that we all known already, but this is going to be the time that he will pass Maradona and win finally a World Cup title with the Argentina National Team?"
+            altDesc="Messi with a big smile after going to the World Cup Final"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="w-[60rem]">
+          <ImportantNews
+            bannerUrl="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2022/03/26/16482599142258.jpg"
+            title="Will Messi finally win a World Cup Title?"
+            description="That Messi is one of the best players in the world is something that we all known already, but this is going to be the time that he will pass Maradona and win finally a World Cup title with the Argentina National Team?"
+            altDesc="Messi with a big smile after going to the World Cup Final"
+          />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
--- src/components/ImportantNews.tsx
+++ src/components/ImportantNews.tsx
@@ -7,22 +7,23 @@ interface ImportantNewsProps {
 
 export default function importantNews(props: ImportantNewsProps) {
   return (
-    <div className="container mx-auto mt-4 relative hover:scale-105 duration-1000">
+    <div className=" mx-auto mt-2 relative hover:scale-105 duration-1000 py-8">
       <a
         href="https://www.marca.com/en/world-cup/2022/03/26/623e73d046163f4b128b4594.html"
         target="_blank"
+        className="flex justify-center"
       >
         <img
           src={props.bannerUrl}
           alt={props.altDesc}
-          className="rounded-xl shadow-primary_alpha shadow-xl max-w-5xl mx-auto"
+          className="rounded-xl mb-10 m-2-5xl shadow-[10px_10px_5px] shadow-primary"
         />
       </a>
 
-      <h1 className="absolute top-10 left-44 text-4xl text-background font-semibold">
+      <h1 className="absolute top-16 left-12 text-4xl text-background font-semibold">
         {props.title}
       </h1>
-      <p className="absolute bottom-10 max-w-lg left-44 text-background text-2xl font-semibold">
+      <p className="absolute bottom-24 max-w-lg left-12 text-background text-2xl font-semibold">
         {props.description}
       </p>
     </div>
